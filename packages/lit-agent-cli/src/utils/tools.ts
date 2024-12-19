5570fad1d74bbde1a59ac1d6656f5213555f3550
Wyatt Barnes <me@wyatt.email>
2024-12-19 00:50:44 -1000

    Bug fix: remove deno from lit-agent-tool-uniswap

--- packages/lit-agent-cli/src/utils/tools.ts
+++ packages/lit-agent-cli/src/utils/tools.ts
@@ -1,14 +1,15 @@
-// import {
-//   uniswapMetadata,
-//   uniswapLitActionDescription,
-// } from "lit-agent-tool-uniswap";
+import {
+  uniswapMetadata,
+  uniswapLitActionDescription,
+} from "lit-agent-tool-uniswap";
 
-const uniswapMetadata = {
-  uniswapLitAction: {
-    IpfsHash: "QmQ9611111111111111111111111111111111111111111111111",
-  },
-};
-const uniswapLitActionDescription = "Uniswap Lit Action";
+// const uniswapMetadata = {
+//   uniswapLitAction: {
+//     IpfsHash: "QmQ9611111111111111111111111111111111111111111111111",
+//   },
+// };
+// const uniswapLitActionDescription = "Uniswap Lit Action";
+
 export interface LitAgentTool {
   name: string;
   description: string;
