ccbd8cb726c2e1e2d05fd82c1893816afdf28917
Wyatt Barnes <me@wyatt.email>
2024-12-20 22:27:16 -1000

    WIP yarn build

--- packages/lit-agent-tool-signer/dist/index.d.ts
+++ packages/lit-agent-tool-signer/dist/index.d.ts
@@ -1,18 +1,14 @@
-
 export type SignerMetadata = {
-  IpfsHash: string;
-  PinSize: number;
-  Timestamp: string;
-  isDuplicate: boolean;
-  Duration: number;
+    IpfsHash: string;
+    PinSize: number;
+    Timestamp: string;
+    isDuplicate: boolean;
+    Duration: number;
 };
-
 export type SignerLitActionString = string;
-
-export declare const signerLitActionDescription: string;
+export declare const signerLitActionDescription = "Lit Action for signing arbitrary messages";
 export declare const signerLitAction: SignerLitActionString;
 export declare const signerMetadata: {
-  signerLitAction: SignerMetadata;
+    signerLitAction: SignerMetadata;
 };
-
 export * from "./policy";
